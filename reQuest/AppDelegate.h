8015b901bdecbf4c877371374c81f81718c410e8
Vinu Ilangovan <vinuilangovan@dhcp-10-101-73-69.wireless.northwestern.private>
2014-11-22 13:54:27 -0600

    Initial Commit

---
+++ reQuest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  reQuest
+//
+//  Created by Vinu Ilangovan on 11/22/14.
+//  Copyright (c) 2014 Vinu Ilangovan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
